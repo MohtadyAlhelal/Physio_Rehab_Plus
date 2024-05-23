4a4b74148ac68ed818af8783d053774eb1aac858
joker1421 <mahdi2000joker@gmail.com>
2024-05-23 12:29:43 +0300

    change setting

--- Clinic/settings.py
+++ Clinic/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2jla_(2s5l-4_984=8)o+y)l-%z%*z9dquplub7vfcmu_8r6&e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
