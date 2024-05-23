a206599af894329e14d507aea5c94befbffb2c0a
joker1421 <mahdi2000joker@gmail.com>
2024-05-23 12:35:23 +0300

    change setting

--- Clinic/settings.py
+++ Clinic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2jla_(2s5l-4_984=8)o+y)l-%z%*z9dquplub7vfcmu_8r6&e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
